b2d93a26333ce011edd4678493c8180ab4cb44a0
nishithak212 <knishitha95@gmail.com>
2025-12-28 17:41:44 -0500

    Handling wait using Implict wait

--- implicitExplictWaits.py
+++ implicitExplictWaits.py
@@ -7,6 +7,13 @@ from selenium.webdriver.support.ui import Select
 service_obj = Service("C:/Users/nishi/Documents/chromedriver-win64/chromedriver-win64/chromedriver.exe")
 driver=webdriver.Chrome(service=service_obj)
 
+#Implicit wait is like a global timeout. i.e. it applies to each and every line in the code
+#Here we mentioned 5 seconds. i.e. 5 seconds is the max timeout. If an element is found within 2 seconds, then it proceeds without waiting for the additional 3 seconds
+driver.implicitly_wait(5)
+
+#Problem with sleep is the script waits for 2 seconds even if the element is found in fraction of seconds
+
+
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.find_element(By.CSS_SELECTOR,".search-keyword").send_keys("ber")
 time.sleep(2)
@@ -27,22 +34,19 @@ driver.find_element(By.CSS_SELECTOR,"img[alt='Cart']").click()
 #Proceed to checkout
 driver.find_element(By.XPATH,"//button[text()='PROCEED TO CHECKOUT']").click()
 
-#But as there is some wait for the system to load and display the text we need to apply wait
-time.sleep(3)
+#But as there is some wait for the system to load and display the text we need to apply sleep
+#time.sleep(3)
 
 #Apply promo code and click apply
 driver.find_element(By.CSS_SELECTOR,"input[class='promoCode']").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR,".promoBtn").click()
 
 #To verify if promo code is applied successfully.
-#But as there is some wait for the system to load and display the text we need to apply wait
-time.sleep(10)
+#But as there is some sleep  for the system to load and display the text we need to apply wait
+#time.sleep(10)
 promoCodeApplied =  driver.find_element(By.CLASS_NAME,"promoInfo")
 assert promoCodeApplied.text == "Code applied ..!"
 print(promoCodeApplied.text)
-
-
-
 
 
 
